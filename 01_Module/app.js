87193d070ca3cdec232d423fc98f420226176d30
GuruPruthviJM <gurupruthvi61@gmail.com>
2024-12-29 18:36:13 +0530

    .

---
+++ 01_Module/app.js
@@ -0,0 +1,11 @@
+// const add = require("./math")
+
+// We need to mention the same name as per the export file.
+// const { add, mul, div, subs } = require("./math")
+
+// Another Method                               
+const math = require("./math")
+
+console.log(math.add(5, 10));
+
+// common js
---
+++ 01_Module/math.js
@@ -0,0 +1,22 @@
+const add = (num1, num2)=>{
+    return num1+num2
+}
+
+const subs = (num1, num2)=>{
+    return num1-num2
+}
+
+const div = (num1, num2)=>{
+    return num1/num2
+}
+
+const mul = (num1, num2)=>{
+    return num1*num2
+}
+
+// module.exports.add = add
+// module.exports.subs =subs
+
+module.exports={
+    add, mul, div, subs
+}
